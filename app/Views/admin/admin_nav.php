1038ef301de0dea9b0b3bf444d6a60e1c8d1254b
faizaar <220605110001@student.uin-malang.ac.id>
2025-05-11 20:09:57 +0700

    meteor

--- app/Views/admin/admin_nav.php
+++ app/Views/admin/admin_nav.php
@@ -107,19 +107,34 @@
       </li>
 
       <li class="nav-item">
-        <a href="<?php echo base_url('TerbitTenggelam'); ?>" target="_blank"
-          class="nav-link d-flex justify-content-between">
+        <span
+          class="nav-link  collapsed  d-flex justify-content-between align-items-center"
+          data-bs-toggle="collapse" data-bs-target="#submenu-pages">
           <span>
             <span class="sidebar-icon">
-              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                  clip-rule="evenodd"></path>
-              </svg>
-            </span>
+              <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
+            </span> 
             <span class="sidebar-text">Meteografi</span>
           </span>
-        </a>
+          <span class="link-arrow">
+            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+          </span>
+        </span>
+        <div class="multi-level collapse " role="list"
+          id="submenu-pages" aria-expanded="false">
+          <ul class="flex-column nav">
+            <li class="nav-item">
+              <a class="nav-link" href="../../pages/examples/sign-in.html">
+                <span class="sidebar-text">Temperatur</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../../pages/examples/sign-up.html">
+                <span class="sidebar-text">Udara</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </li>
 
       <li class="nav-item">
