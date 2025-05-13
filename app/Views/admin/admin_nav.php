601439894b5c1a29fcbeaadcbd64922dc623ce99
renrenn04 <renataamalia34@gmail.com>
2025-05-13 17:52:26 +0700

    Update admin_nav.php

--- app/Views/admin/admin_nav.php
+++ app/Views/admin/admin_nav.php
@@ -29,7 +29,9 @@
         </a>
       </div>
     </div> -->
-    <ul class="nav flex-column pt-3 pt-md-0">
+
+    <!-- VOLT (DIHAPUS AJA) -->
+    <!-- <ul class="nav flex-column pt-3 pt-md-0">
       <li class="nav-item">
         <a href="../../index.html" class="nav-link d-flex align-items-center">
           <span class="sidebar-icon">
@@ -46,7 +48,7 @@
               <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
               <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
             </svg>
-          </span>
+          </span> -->
           <span class="sidebar-text">Dashboard</span>
         </a>
       </li>
