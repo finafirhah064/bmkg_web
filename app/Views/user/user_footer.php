23effa257e69c4b3ea714ff8dbc502fb742ad38f
Arumpuspit <aparumpuspita@gmail.com>
2025-05-28 23:11:27 +0700

    Dashboard Baru User

--- app/Views/user/user_footer.php
+++ app/Views/user/user_footer.php
@@ -1,124 +1,73 @@
-<footer class="footer py-6 bg-gray-800 text-white">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <img class="navbar-brand-dark mb-4" height="35" src="../../assets/img/brand/light.svg" alt="Logo light">
-                <p>Volt is a Premium Bootstrap 5 Admin Dashboard bringing together beautiful UI/UX design and functional elements.</p>
-                <ul class="social-buttons mb-5 mb-lg-0">
-                    <li>
-                        <a href="https://twitter.com/themesberg" aria-label="twitter social link" class="icon-white me-2">
-                            <span class="fab fa-twitter"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.facebook.com/themesberg/" class="icon-white me-2" aria-label="facebook social link">
-                            <span class="fab fa-facebook"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/themesberg" aria-label="github social link" class="icon-white me-2">
-                            <span class="fab fa-github"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://dribbble.com/themesberg" class="icon-white" aria-label="dribbble social link" >
-                            <span class="fab fa-dribbble"></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-6 col-md-2 mb-5 mb-lg-0">
-                <span class="h5">Themesberg</span>
-                <ul class="links-vertical mt-2">
-                    <li><a target="_blank" href="https://themesberg.com/blog">Blog</a></li>
-                    <li><a target="_blank" href="https://themesberg.com/products">Products</a></li>
-                    <li><a target="_blank" href="https://themesberg.com/about">About Us</a></li>
-                    <li><a target="_blank" href="https://themesberg.com/contact">Contact Us</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md-2 mb-5 mb-lg-0">
-                <span class="h5">Other</span>
-                <ul class="links-vertical mt-2">
-                    <li><a href="https://themesberg.com/docs/volt-bootstrap-5-dashboard/getting-started/quick-start/" target="_blank">Docs
-                            <span class="badge badge-sm bg-secondary text-dark ms-2">v1.4</span></a></li>
-                    <li><a href="https://themesberg.com/docs/volt-bootstrap-5-dashboard/getting-started/changelog/" target="_blank">Changelog</a></li>
-                    <li><a target="_blank" href="https://themesberg.com/licensing">License</a>
-                    </li>
-                    <li><a target="_blank" href="https://themesberg.com/contact">Support</a></li>
-                </ul>
-            </div>
-            <div class="col-12 col-md-4 mb-5 mb-lg-0">
-                <span class="h5 mb-3 d-block">Subscribe</span>
-                <form action="#">
-                    <div class="form-row mb-2">
-                        <div class="col-12">
-                            <input type="email" class="form-control mb-2" placeholder="example@company.com" name="email" aria-label="Subscribe form" required>
-                        </div>
-                        <div class="col-12 d-grid">
-                            <button type="submit" class="btn btn-secondary" data-loading-text="Sending">
-                                <span>Subscribe</span>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-                <p class="text-muted font-small m-0">We’ll never share your details. See our <a class="text-white" href="#">Privacy Policy</a></p>
-            </div>
+<footer class="footer py-4 bg-white text-dark border-top mt-5">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+
+    <footer class="text-center">
+        <div class="container d-flex flex-column align-items-center">
+            <img src="./assets/img/bmkg-logo.png" alt="BMKG" class="logo mb-3">
+            <h5 class="fw-bold">KONTAK KAMI</h5>
+            <p><i class="fas fa-map-marker-alt"></i> Jl. Bend. Lahor No.40, Karangkates, Kec. Sumberpucung, Kabupaten Malang, Jawa Timur 65165</p>
+            <p><i class="fas fa-phone-alt"></i> 081232087312</p>
+            <p><i class="fas fa-envelope"></i> info@bmkg-karangkates.go.id</p>
+            <hr class="bg-white w-100">
+            <p class="mt-3">&copy; 2025 - Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) Karangkates. PKL UIN MALANG.</p>
         </div>
-        <hr class="bg-gray-700 my-5">
-        <div class="row">
-            <div class="col mb-md-0">
-                <a href="https://themesberg.com" target="_blank" class="d-flex justify-content-center">
-                    <img src="../../assets/img/themesberg-logo-alt.svg" height="35" class="mb-4" alt="Themesberg Logo">
-                </a>
-            <div class="d-flex text-center justify-content-center align-items-center" role="contentinfo">
-                <p class="fw-normal font-small mb-0">Copyright © PKL UIN Malang 2025-<span class="current-year">2021</span>. All rights reserved.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <style>
+        .footer ul li i {
+            width: 20px;
+            text-align: center;
+            color: #9ab;
+        }
+    </style>
+
+
 
     <!-- Core -->
-<script src="./vendor/@popperjs/core/dist/umd/popper.min.js"></script>
-<script src="./vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./vendor/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="./vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 
-<!-- Vendor JS -->
-<script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
+    <!-- Vendor JS -->
+    <script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
 
-<!-- Slider -->
-<script src="./vendor/nouislider/distribute/nouislider.min.js"></script>
+    <!-- Slider -->
+    <script src="./vendor/nouislider/distribute/nouislider.min.js"></script>
 
-<!-- Smooth scroll -->
-<script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <!-- Smooth scroll -->
+    <script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
 
-<!-- Charts -->
-<script src="./vendor/chartist/dist/chartist.min.js"></script>
-<script src="./vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <!-- Charts -->
+    <script src="./vendor/chartist/dist/chartist.min.js"></script>
+    <script src="./vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
 
-<!-- Datepicker -->
-<script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
+    <!-- Datepicker -->
+    <script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
 
-<!-- Sweet Alerts 2 -->
-<script src="./vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <!-- Sweet Alerts 2 -->
+    <script src="./vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
 
-<!-- Moment JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+    <!-- Moment JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
 
-<!-- Vanilla JS Datepicker -->
-<script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
+    <!-- Vanilla JS Datepicker -->
+    <script src="./vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
 
-<!-- Notyf -->
-<script src="./vendor/notyf/notyf.min.js"></script>
+    <!-- Notyf -->
+    <script src="./vendor/notyf/notyf.min.js"></script>
 
-<!-- Simplebar -->
-<script src="./vendor/simplebar/dist/simplebar.min.js"></script>
+    <!-- Simplebar -->
+    <script src="./vendor/simplebar/dist/simplebar.min.js"></script>
 
-<!-- Github buttons -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-<!-- Volt JS -->
-<script src="./assets/js/volt.js"></script>
+    <!-- Volt JS -->
+    <script src="./assets/js/volt.js"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
