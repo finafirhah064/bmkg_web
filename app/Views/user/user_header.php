b97844d0004cad608761546fbf544b0202167ab2
LFadhilla <urgirlfadyl@gmail.com>
2025-05-29 20:34:38 +0700

    Update user_header.php

--- app/Views/user/user_header.php
+++ app/Views/user/user_header.php
@@ -197,8 +197,8 @@
     <!-- Bootstrap JS (required for responsive navbar) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Bootstrap 5 -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script> -->
 
 </body>
 
